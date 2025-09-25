f364ec544a0bf2c99e25e2854b9487c7445eb649
jhonatasfender <jhonatas.fender@gmail.com>
2025-09-25 15:33:33 -0300

    feat: integrate Swagger documentation setup in Vercel configuration
    - Added SwaggerModule configuration in vercel.ts to generate API documentation.
    - Configured API title, description, version, and authentication details for improved developer experience.

--- apps/api/src/vercel.ts
+++ apps/api/src/vercel.ts
@@ -1,11 +1,38 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 let cachedExpress: any;
 
 async function bootstrap(): Promise<any> {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true, credentials: true });
+
+  const config = new DocumentBuilder()
+    .setTitle("PDPX API")
+    .setDescription("Projeto PDPX - API")
+    .setVersion("1.0")
+    .addTag("auth", "Endpoints de autenticação")
+    .addBearerAuth(
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "JWT",
+        description: "Enter JWT token",
+        in: "header",
+      },
+      "JWT-auth",
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   await app.init();
   return app.getHttpAdapter().getInstance();
 }
